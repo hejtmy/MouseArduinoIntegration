f7efa0c664e4b956b5d3d08a88ffaf13e313c64c
hejtmy <hejtmy@gmail.com>
2015-12-10 17:19:08 +0100

    Adds arduino class

--- Experiment/arduino.py
+++ Experiment/arduino.py
@@ -1,15 +1,22 @@
-##connect to arduino
-def connect():
-    global ard
-    try:
-        ard = serial.Serial("COM3", 9600)
-    except Exception:
-        raise ValueError("COULDN'T CONNECT TO ARDUINO!!!")
+import serial               ##for communication with arduino
+class Arduino:
 
-##sends stop code "STOP" to let arduino know communication is OK
-def sendStop():
-    ard.write(b'STOP')
 
-##at the end of the experiment, reset arduino cycle = start sending "CX37"" again
-def sendReset():
-    ard.write(b'REPEAT')
+    def __init__(self, baudrate = 9600, port = "COM3"):
+        self.connection = serial.Serial();
+        self.connection.baudrate = baudrate;
+        self.connection.port = port;
+    ##connect to arduino
+    def connect(self):
+        try:
+            self.connection.open();
+        except Exception:
+            raise ValueError("COULDN'T CONNECT TO ARDUINO!!!")
+
+    ##sends stop code "STOP" to let arduino know communication is OK
+    def sendConnectionOK(self):
+        self.connection.write(b'STOP')
+
+    ##at the end of the experiment, reset arduino cycle = start sending "CX37"" again
+    def sendReset(self):
+        self.connection.write(b'REPEAT')
