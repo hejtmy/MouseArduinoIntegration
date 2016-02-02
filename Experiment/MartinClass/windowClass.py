9d6becd004460f91ff3d8fd6e5642dc4913784c8
Hejtmar <Hejtmar@gmail.com>
2016-02-02 21:14:49 +0100

    windowClass update
    Working example of manipulation with window and images - changing
    images, minimalize / maximize, ...

--- Experiment/MartinClass/windowClass.py
+++ Experiment/MartinClass/windowClass.py
@@ -5,66 +5,78 @@ class window(Tk, Canvas):
     def __init__(self, parent):
         Tk.__init__(self, parent)
         self.parent = parent
+        self.geometry("%dx%d+0+0" % (self.winfo_screenwidth() / 2.0, self.winfo_screenheight() / 2.0))
         self.grid()
-        self.geometry("%dx%d+0+0" % (self.winfo_screenwidth(), self.winfo_screenheight()))
         
-        self.path1 = r"D:\Git\MouseArduinoIntegration\Experiment\circle.gif"
-        self.path2 = r"D:\Git\MouseArduinoIntegration\Experiment\triangle.gif"
+        self.currentImg = None
+        self.whiteImg = PIM(file = r"D:\Git\MouseArduinoIntegration\Experiment\white.gif")
         
-        self.img1 = PIM(file = self.path1)
-        self.img1_width = self.img1.width()
-        self.img1_height = self.img1.height()
-        
-        self.img2 = PIM(file = self.path2)
-        self.img2_width = self.img2.width()
-        self.img2_height = self.img2.height()
-        
-
-        self.myCanvas = Canvas(parent, bg = "white", height = self.winfo_screenheight(), width = self.winfo_screenwidth())        
-#        self.myCanvas = Canvas(parent, height = self.winfo_screenheight(), width = self.winfo_screenwidth())
-        
+        self.myCanvas = Canvas(parent, bg = "white", height = self.winfo_screenheight(), width = self.winfo_screenwidth())
         self.myCanvas.grid(column = 0, row = 0) #sticky = "EW")
         
-        self.canvasImg = self.myCanvas.create_image(self.winfo_screenwidth() / 2.0, self.winfo_screenheight() / 2.0, image = self.img1)
-#        self.canvasImg = self.myCanvas.create_image(self.img1_width, self.img1_height, image = self.img1)
+        self.canvasImg = self.myCanvas.create_image(self.winfo_screenwidth() / 2.0, self.winfo_screenheight() / 2.0, image = self.whiteImg)
         
-#        self.x = img_width / 2.0
-#        self.y = img_height / 2.0
+        self.update()
+
+    def maximize(self):
+        self.attributes('-fullscreen', True)
+        self.deiconify()
+        self.update()
         
+    def topmost(self):
+        self.attributes('-topmost', True)
+        self.update()
         
-    def change_img(self):
-        self.myCanvas.itemconfig(self.canvasImg, image = self.img2)
+    def changeImg(self, path):
+        self.currentImg = PIM(file = "%s" %path)
+        self.myCanvas.itemconfig(self.canvasImg, image = self.currentImg)
+        self.update_idletasks()
+    
+    def showWhite(self):
+        self.myCanvas.itemconfig(self.canvasImg, image = self.whiteImg)
+        self.update_idletasks()
         
     def minimize(self):
-        self.wm_state('iconic')
+        self.iconify()
+        self.update()
         
-    def show_blank(self):
-#        self.image_to_show = PIM(path to folder + "blank.gif")
-        self.myCanvas.itemconfig(self.canvasImg, image = self.img2)
+
         
 if __name__ == "__main__":
     
     myWindow = window(None)
+ 
+    start = time.clock()
+        
+    while ((time.clock() - start) < 1):
+        pass
+    myWindow.maximize()
+    myWindow.topmost()
     
-    myWindow.attributes('-topmost', True)
-    myWindow.attributes('-fullscreen', True)
-#    myWindow.lift()
+    while ((time.clock() - start) < 2):
+        pass  
+    myWindow.changeImg("D:\Git\MouseArduinoIntegration\Experiment\circle.gif")  
     
-    myWindow.update()
-    time.sleep(2)
+    while ((time.clock() - start) < 3):
+        pass
+    myWindow.minimize()
     
-    myWindow.change_img()
-    myWindow.update()
-    time.sleep(2)
+    while ((time.clock() - start) < 4):
+        pass
+    myWindow.maximize()
     
-    x = 0
-    while (x < 100):
-        myWindow.myCanvas.move(myWindow.canvasImg, 10, 0)
-        myWindow.update()
-        time.sleep(0.01)
-        x += 1
+    while ((time.clock() - start) < 5):
+        pass
+    myWindow.changeImg("D:\Git\MouseArduinoIntegration\Experiment\square.gif")
     
-    myWindow.destroy()
+    while ((time.clock() - start) < 6):
+        pass
+    myWindow.showWhite()
+    
+    while ((time.clock() - start) < 7):
+        pass
+    myWindow.destroy() 
+
 #1 - initiate minimalized window object with proper parametres = window size, single container etc.
 #2 - set functions for showBlank, showImage(circle.jpg / square.jpg / ...), closeWindow
 #3 - 
